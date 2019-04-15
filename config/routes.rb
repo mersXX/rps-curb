a8066c256da629251d9f68dfb1d6015b7192a4f5
mersXX <m.proshich@outlook.com>
2019-04-15 11:22:30 +0300

    add games controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'games#index'
+
+  get 'games/index'
+  get '/throw' => 'games#throw', as: '/throw'
 end
